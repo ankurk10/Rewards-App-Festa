b3b745c782e95ee62b7110bea2aeb5d48bfb8371
ankurk10 <ankurkumarkushwaha.10@gmail.com>
2021-10-16 11:31:15 +0530

    Added benefits and career screens

--- lib/benefits_screen.dart
+++ lib/benefits_screen.dart
@@ -5,8 +5,64 @@ class BenefitsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: MediaQuery.of(context).size.height, color: Colors.yellow
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Center(
+                heightFactor: 4,
+                child: Text(
+                  "My Cash",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+
+              Center(
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  color: Colors.white,
+                  elevation: 2,
+                  child: Container(
+                    height: 400,
+                    width: 400,
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+
+                    child: const Padding(
+                      padding: EdgeInsets.all(12),
+                      child: Text(
+                        'Health Insurance : 5,00,000 \n\nAccident Cover : 15,00,000 \n\nLife Cover : 10,00,000 \n\n EPF : 21,000 \n\n EDLI : 21,000 \n\n Gratuity : 21,000 \n\n Internet : 21,000 \n\n Leave Encashment : 21,000' ,
+                        maxLines: 20,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+
+              SizedBox(
+                height: 20,
+                width: 20,
+              ),
+
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/career_screen.dart
+++ lib/career_screen.dart
@@ -5,8 +5,78 @@ class CareerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: MediaQuery.of(context).size.height, color: Colors.teal
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: [
+              const Center(
+                heightFactor: 5,
+                child: Text(
+                  "My Career",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 35,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+
+
+              SizedBox(
+                height: 300,
+                width: 20,
+              ),
+
+              Container(
+                padding: EdgeInsets.only(top: 3, left: 3),
+                decoration:
+                BoxDecoration(
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border(
+                      bottom: BorderSide(color: Colors.black),
+                      top: BorderSide(color: Colors.black),
+                      left: BorderSide(color: Colors.black),
+                      right: BorderSide(color: Colors.black),
+
+
+
+                    )
+
+                ),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  height: 60,
+                  onPressed: () {
+                  },
+                  color: Colors.teal,
+                  elevation: 0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+
+                  ),
+                  child: Text(
+                    "Sign up", style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18,
+                    color: Colors.white,
+
+                  ),
+                  ),
+
+
+                ),
+
+
+
+              ),
+            ],
+          ),
+        ),
+      ),
+
     );
   }
 }
